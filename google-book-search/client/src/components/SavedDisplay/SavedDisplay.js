2040e4584414129c579345796946038824aa3df8
agatha12 <agkiecun@gmail.com>
2019-02-10 22:05:38 -0500

    add working

---
+++ google-book-search/client/src/components/SavedDisplay/SavedDisplay.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./SavedDisplay.css"
+
+function SearchForm(props) {
+  return (
+<div className="bookdisplay">
+    <div className="book">
+      <h4>{props.title}</h4><br></br>
+      <img className="bookimg" alt="not available" src={props.image}></img><br></br>
+      <p>Authors: {props.authors}</p>
+      <p>Description: {props.decription}</p>
+      <a href={props.link}>View on Google Books</a><br></br>
+      <button type="button" className="btn btn-primary" onClick={props.deleteBook}>Delete</button>
+    </div>
+</div>
+  );
+}
+
+export default SearchForm;
--- google-book-search/client/src/pages/Saved.js
+++ google-book-search/client/src/pages/Saved.js
@@ -1,12 +1,56 @@
-import React from "react";
+import SavedDisplay from "../components/SavedDisplay/SavedDisplay";
+import API from "../utils/API";
+import React, { Component } from "react"
+class Saved extends Component {
+    state = {
+        books: [
+            {
+                _id: "",
+                title: "",
+                authors: "",
+                description: "",
+                image: "",
+                link: ""
+            }
+        ]
+    }
 
-function Saved (){
+    componentDidMount() {
+        this.loadBooks();
+      }
+    
+      loadBooks = () => {
+        API.getBooks()
+          .then(res =>
+            this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+          )
+          .catch(err => console.log(err));
+      };
+    deleteBook = id => {
+        API.deleteBook(id)
+          .then(res => this.loadBooks())
+          .catch(err => console.log(err));
+      };
 
-    return (
-        <div>
-            Saved Page
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <div>
+                    {this.state.books.map((book, index) => (
+                        <SavedDisplay
+                            key={index}
+                            title={book.title}
+                            authors={book.authors}
+                            decription={book.description}
+                            image={book.image}
+                            link={book.link}
+                            deleteBook={() => this.deleteBook(book._id)}
+
+                        />))}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Saved;
